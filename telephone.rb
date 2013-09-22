386e2fd212d753908bc334bbb0879d9bc7c01b90
Rainbough Phillips <rainbough@gmail.com>
2013-09-22 16:53:55 -0500

    merge

--- telephone.rb
+++ telephone.rb
@@ -25,6 +25,7 @@ end
 # @gif_url is set to the absolute url so that @gif_url can later be 
 # passed to bit.ly API. 
 post '/twitter' do 
+  
   @result = TweetSearch::UserWord.new(params[:word]).search![0]
   @output_result = GiphySearch::PicSearch.new(@result).fetch_gif!
   @gif_url = "http://media.giphy.com/media/#{@output_result}/giphy.gif"
