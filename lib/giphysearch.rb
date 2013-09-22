efbdbd568b81d6bb16cc0a0294699e6150516596
veleary <veleary@gmail.com>
2013-09-21 21:57:37 -0500

    update gemfile

--- lib/giphysearch.rb
+++ lib/giphysearch.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'giphy'
+require 'pry'
 
 require_relative 'tweetsearch'
 
@@ -8,15 +9,18 @@ module GiphySearch
   # Take the tweet word from tweetsearch module and search Giphy for a gif.
   class PicSearch    
     attr_accessor :result
-    
+    def initialize(result)
+      @result = result
+    end
     # Fetch a gif from Giphy with the provided result from the Twitter Search.
     #
     # Returns the first matching Giphy::Gif
     def search!
       client = Giphy::Configuration.configure do |config|
-      config.version = THE_API_VERSION
-      config.api_key = YOUR_API_KEY  
+      # config.version = ""
+      config.api_key = "dc6zaTOxFJmzC" 
       end  
+binding.pry
       @pic = client.search(@result, {limit:1})
     end
   end
